9992b38f75514705eb1149738d49d696a0e5add9
Tommy <temurph1@asu.edu>
2016-05-02 15:58:39 -0700

    Added Current User Page

--- users.php
+++ users.php
@@ -1,8 +1,33 @@
-<?php
+ 
+<h1>Current Users</h1>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<table id="table" border="2">
+      <tr>
+      <th>Username</th>
+      <th>Email</th>
+    </tr>     
 
+ <?php
+      
+      include 'connection.php';
+    $query = "SELECT * FROM users ";
+    $myQuery = mysqli_query($conn, $query);
+   
+    while($row = mysqli_fetch_array($myQuery)){
+     ?>   
+    
+    <form action="userPlaylist.php" method="post">
+        
+    <tr><td><?php echo $row["username"] ?> 
+        </td><td> <?php echo $row["email"] ?> 
+        <td><label>
+    <input type = "hidden" name = "songID" value = "<?php echo $row["id"] ?>" >
+    <input type= "submit" name = "submit" value="View Playlist" />
+    </label>
+    </td></tr>
+    </form>   
+    
+    
+    <?php
+    }
+    ?>
