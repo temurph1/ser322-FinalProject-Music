1f94b46b543e6bb9e7f69671890cf21fc6125456
Tommy <temurph1@asu.edu>
2016-05-02 20:33:33 -0700

    Login page updated (for real this time I promise, netbeans is weird)

---
+++ old/oldlogin.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+		<style>
+		body {
+			background-color: #d0e4fe;
+		}
+
+		h1 {
+			color: orange;
+			text-align: center;
+		}
+
+		</style>
+    </head>
+    <body>
+        <?php
+        ?>
+        
+        
+<h1>Welcome to Music</h1>        
+        
+<form action="loginprocess.php" method="post">
+  Username:<br>
+  <input type="text" name="username"><br>
+  Password:<br>
+  <input type="password" name="password">
+  <br>
+  <input type="submit" name = "submit" value="Submit">
+</form>
+
+<form action="signup.php" method="get">
+    <input type="submit" value="Register" action="signup.php" method="post">
+</form>
+
+
+    </body>
+</html>
