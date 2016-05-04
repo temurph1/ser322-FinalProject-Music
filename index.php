26a66e7044344b81c4a23f037fa9f1dd597962f3
Tommy <temurph1@asu.edu>
2016-05-03 17:48:35 -0700

    Index page looks more groovy

--- index.php
+++ index.php
@@ -2,94 +2,126 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+    <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-    <title>Groovy - A Mediocre Music Platform</title>
+        <title>Groovy - A Mediocre Music Platform</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <style>
-    body {
-        padding-top: 70px;
-        /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
-    }
-    </style>
+        <!-- Custom CSS -->
+        <style>
+            body {
+                padding-top: 80px;
+                background-image:url("images/bggradient.jpg");
+                background-attachment: fixed;
+                /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+            }
+            .panel-transparent {
+                background: none;
+            }
+            .panel-transparent .panel-heading{
+                background: rgba(122, 130, 136, 0.2)!important;
+            }
+            .panel-transparent .panel-body{
+                background: rgba(46, 51, 56, 0.2)!important;
+            }
+        </style>
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
 
-</head>
+    </head>
 
-<body>
+    <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#"><img src="images/groovylogowhite.png" width="105"></a>
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"><img src="images/groovylogowhite.png" width="105"></a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li>
+                            <a href="index.php">Home</a>
+                        </li>
+                        <li>
+                            <a href="login.php">Login</a>
+                        </li>
+                        <li>
+                            <a href="signup.php">Register</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
             </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="index.php">Home</a>
-                    </li>
-                    <li>
-                        <a href="login.php">Login</a>
-                    </li>
-                    <li>
-                        <a href="signup.php">Register</a>
-                    </li>
-                </ul>
+            <!-- /.container -->
+        </nav>
+
+        <!-- Page Content -->
+        <div class="container" align="center">
+
+            <div class="panel panel-primary panel-transparent">
+                <div class="panel-heading" style="padding: 100px">
+                    <h1 class="panel-title" style="font-size:60px; font-weight: bold;">Welcome to a new world of music</h1>
+                    <p class="lead">Click the button below to get started</p>
+                    <form action="signup.php">
+                        <a href="signup.php" button type="button" class="btn btn-lg btn-success" style="width:300px; font-size:15px; ">START LISTENING NOW</button></a><br>
+                        <a href="login.php" button type="button" class="btn btn-link">Existing user? Click here.</button></a>
+                    </form>
+                </div>
             </div>
-            <!-- /.navbar-collapse -->
+            <!-- /.row -->
+
         </div>
         <!-- /.container -->
-    </nav>
 
-    <!-- Page Content -->
-    <div class="container">
+                <div class="container" align="center">
+            <div class="jumbotron" style="width: 100%; height:100%; padding: 100px; background-color: white">
+                <h1 class="panel-title" style="font-size:60px; font-weight: bold; margin:px">WHAT IS GROOVY?</h1>
+                <h1 class="panel-title" style="font-size:20px; font-weight: bold;">Do we even know? Probably not.</h1>
+                <h1 class="panel-title" style="font-size:10px; font-weight: bold;">ALRIGHT. It's a reeeaaalllly mediocre music listening thing. <br> Now here's some gibberish.</h1>
 
-        <div class="row">
-            <div class="col-lg-12 text-center">
-                <h1>Welcome to a new world of music</h1>
-                <p class="lead">Click the button below to get started</p>
-                <form action="signup.php">
-                    <a href="signup.php" button type="button" class="btn btn-lg btn-success" style="width:300px; font-size:15px">START LISTENING NOW</button></a><br>
-                    <a href="login.php" button type="button" class="btn btn-link">Existing user? Click here.</button></a>
-                </form>
+                <h3 style="font-size:16px">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tristique ex at lorem porttitor, in eleifend elit pellentesque. Suspendisse potenti. Praesent at porta lacus. Aenean ac tortor scelerisque, sollicitudin leo sit amet, varius est. Sed auctor sollicitudin urna nec tempus. Aliquam erat volutpat. Vivamus pulvinar diam nec hendrerit viverra. Cras aliquam in diam a cursus.
+                    Suspendisse est risus, rutrum ut commodo quis, mattis id tellus. Pellentesque pharetra elit enim, a molestie nisl laoreet vitae. Donec placerat cursus dolor eu placerat. Cras vitae nisi euismod velit fermentum scelerisque sit amet id nibh. Integer aliquet dui turpis, ut rutrum dui consequat in. Sed finibus lacus eu nulla cursus, at posuere orci vulputate. Aenean at enim id urna facilisis ultricies. Vestibulum tristique mi eu lacus commodo, non convallis odio efficitur. Donec euismod orci ac turpis pharetra, in volutpat urna tincidunt. In eu lorem vel ipsum dapibus efficitur. Proin porta massa quis sapien tristique tempor. Vivamus et sapien quis dolor feugiat pellentesque. Duis vel semper enim. Ut sagittis leo nec nunc imperdiet, ut luctus felis facilisis. Vestibulum lacinia accumsan nunc ut tincidunt.
+                    Duis porta congue nisi ut auctor. Nulla id felis id orci ullamcorper rutrum at non justo. Suspendisse pharetra quis metus ac convallis. Integer vehicula erat ac purus euismod, ac semper libero ultrices. In at convallis libero. Praesent felis orci, gravida ultricies ipsum in, semper molestie justo. Suspendisse sit amet convallis nisi. Phasellus id venenatis nunc. Nullam eget nunc pharetra, congue justo eget, ullamcorper leo. Aliquam vitae sem mi. Nunc mollis eu tellus euismod mollis. Phasellus lacinia fringilla massa et facilisis. Integer pulvinar quis lorem vel cursus. Sed porta orci non lacus ornare, vitae aliquet justo congue. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                    Phasellus at luctus est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque bibendum tristique est in porta. Nunc fringilla laoreet pulvinar. Vestibulum et elit sed dui porttitor fringilla. Nam et pulvinar purus. Integer a lorem at sem finibus fringilla vel nec mauris. Vestibulum vitae lacus quis lectus viverra porttitor nec vel dolor. Sed venenatis fringilla semper. Vivamus molestie porta molestie. Nam ornare, ex sed blandit aliquam, odio ligula viverra massa, id ultricies diam tellus vel dolor. Cras hendrerit euismod sapien. Quisque feugiat justo ut magna interdum dapibus.
+                    Donec in laoreet mi. Nulla sapien metus, iaculis a erat vitae, scelerisque maximus nibh. Aenean in fringilla sapien. Sed at erat pharetra, interdum quam sed, eleifend sem. Integer vitae felis augue. Quisque placerat posuere tellus, at congue nisl imperdiet vitae. Donec ultricies dui nibh, luctus convallis ex rutrum id. Cras ut rutrum dui, quis elementum eros. Curabitur in velit in est venenatis varius nec ac sem. Aenean quis posuere sapien, eu venenatis ligula. Aliquam accumsan velit neque, vitae sagittis nisi sagittis quis.
+                    Etiam rhoncus viverra risus. Etiam auctor sem a turpis tristique, id porttitor leo lacinia. Phasellus in nulla diam. Integer porttitor lorem vitae elementum tempus. Cras tincidunt, lorem ut tempor tincidunt, est est elementum felis, in fringilla arcu dui eget massa. In hac habitasse platea dictumst. Curabitur semper tellus at porttitor accumsan. Fusce feugiat nisl enim, et convallis diam semper ac. Fusce ut pellentesque urna, eu euismod elit. Maecenas id velit quis nisi porttitor placerat. Pellentesque sem neque, facilisis ac imperdiet et, gravida quis urna. Phasellus rhoncus non velit eget mattis.</h3>
+
+
+
+
             </div>
+
+
         </div>
-        <!-- /.row -->
 
-    </div>
-    <!-- /.container -->
+        <!-- jQuery Version 1.11.1 -->
+        <script src="js/jquery.js"></script>
 
-    <!-- jQuery Version 1.11.1 -->
-    <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-</body>
+    </body>
 
 </html>
